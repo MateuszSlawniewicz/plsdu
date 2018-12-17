dafe1dc6237623aa2a3577026315b0a39a376054
MateuszSlawniewicz <mateusz.slawniewicz@gmail.com>
2018-12-17 03:02:43 +0100

    four commit

--- src/main/java/day4/ForsExample2.java
+++ src/main/java/day4/ForsExample2.java
@@ -6,6 +6,11 @@ public class ForsExample2 {
     public static void main(String[] args) {
 
         christmasTree(3);
+        christmasTreeUpsidedown(3);
+        System.out.println();
+        printNumbersInTower(3);
+
+        printAnotherTowerOfNumbers(5);
     }
 
     public static void christmasTree(int param) {
@@ -18,5 +23,57 @@ public class ForsExample2 {
         }
 
 
+    }
+
+    public static void christmasTreeUpsidedown(int param) {
+
+        for (int i = param - 1; i >= 0; i--) {
+            for (int j = 0; j < i + 1; j++) {
+                System.out.print(i + 1);
+            }
+            System.out.println();
+        }
+
+
+    }
+
+    private static void printNumbersInTower(int param) {
+
+
+        for (int i = 0; i < param; i++) {
+            int num = 1;
+            for (int j = 0; j < i + 1; j++) {
+
+
+                System.out.print(num);
+                num++;
+            }
+            System.out.println();
+        }
+
+
+    }
+
+
+    private static void printAnotherTowerOfNumbers(int param) {
+
+
+        for (int i = 0; i < param; i++) {
+
+
+            for (int j =(param - i-1); j >0; j--) {
+                // printing spaces
+                System.out.print(" ");
+            }
+            int num = 1;
+
+            for (int j = 0; j <= i; j++) {
+
+                System.out.print(num);
+                num++;
+            }
+
+            System.out.println();
+        }
     }
 }
