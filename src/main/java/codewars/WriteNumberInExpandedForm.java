011e51555693298131b587780f3d0e421313227a
MateuszSlawniewicz <mateusz.slawniewicz@gmail.com>
2019-01-11 16:13:59 +0100

    codewars commit second

--- src/main/java/codewars/WriteNumberInExpandedForm.java
+++ src/main/java/codewars/WriteNumberInExpandedForm.java
@@ -22,25 +22,39 @@ public class WriteNumberInExpandedForm {
 
     private static String expandedForm(int num) {
 
-        String[] str = Integer.toString(num).split("");
-        String result;
-        List<String> l = new ArrayList<String>();
 
-        for(int i = 0; i < str.length; i++){
-            if(Integer.valueOf(str[i]) > 0){
-                String s = str[i];
-                for(int j = i; j < str.length - 1; j++){
-                    s += '0';
-                }
-                l.add(s);
-            }
+        String outs = "";
+        for (int i = 10; i < num; i *= 10) {
+            int rem = num % i;
+            outs = (rem > 0) ? " + " + rem + outs : outs;
+            num -= rem;
         }
+        outs = num + outs;
 
-        result = l.toString();
-        result = result.substring(1, result.length() - 1).replace(",", " +");
-        System.out.println(result);
+        return outs;
 
-        return result;
+
+// inne rozwiązanie
+//        String[] str = Integer.toString(num).split("");
+//        String result;
+//        List<String> l = new ArrayList<String>();
+
+//        for(int i = 0; i < str.length; i++){
+//            if(Integer.valueOf(str[i]) > 0){
+//                String s = str[i];
+//                for(int j = i; j < str.length - 1; j++){
+//                    s += '0';
+//                }
+//                l.add(s);
+//            }
+//        }
+
+//        result = l.toString();
+//        result = result.substring(1, result.length() - 1).replace(",", " +");
+//        System.out.println(result);
+
+//        return result;
+
 
     }
 }
